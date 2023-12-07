0a2b12973e10182850632961a455b7d6a7432f39
Alberto Carrasco del Cid <albertocarrasco.dc@gmail.com>
2023-12-07 13:00:18 +0100

    Initial Commit

---
+++ MyInventory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyInventory
+//
+//  Created by Juan Antonio Carrasco del Cid on 7/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
